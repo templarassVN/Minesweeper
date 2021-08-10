d06046bc3d1cb7325abe386580399acaa189aebb
templarassVN <53231722+templarassVN@users.noreply.github.com>
2021-08-10 21:07:43 +0700

    Done Gameover Yet Victory

---
+++ src/states/BaseState.lua
@@ -0,0 +1,20 @@
+BaseState = Class{}
+
+function BaseState:init( ... )
+	-- body
+end
+
+function BaseState:enter( enterParams )
+	-- body
+end
+
+function BaseState:exit()
+end
+
+function BaseState:update( dt )
+	-- body
+end
+
+function BaseState:render( ... )
+	-- body
+end
